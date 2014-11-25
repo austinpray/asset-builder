8cd7d51e7dc945a5960bfb0ff90da7b4ba489084
Austin Pray <austin@austinpray.com>
2014-11-24 18:45:58 -0600

    Adds basic tests

--- test/test.js
+++ test/test.js
@@ -1,10 +1,39 @@
 /* jshint node: true */
-/* global describe, it */
+/* global describe, it, beforeEach */
 'use strict';
 
-var should = require('chai').should();
+var assert = require('chai').assert;
 var fs = require('fs');
-var path = require('path');
-var useref = require('../lib/index');
+var m  = require('../lib/index');
+var bower = require('bower');
+var Q = require('q');
 
-console.log(useref('./test/fixtures/manifest.json'));
+function bowerSetup(bowerJson) {
+  bowerJson = bowerJson || 'bower.json';
+  var deferred = Q.defer();
+  fs.writeFileSync('test/tmp/bower.json', fs.readFileSync('test/fixtures/'+bowerJson));
+  bower.commands.prune().on('end', function () {
+    bower.commands.install(null, null, { 'cwd': 'test/tmp/' }).on('end', function () {
+      deferred.resolve();  
+    });
+  });
+  return deferred.promise;
+}
+
+describe('Properties', function(){
+  var manifest;
+  beforeEach(function(done){
+    bowerSetup().then(function () {
+      manifest = m('./fixtures/manifest.json', {
+        bowerJsonDirectory: './tmp/',
+      });
+      done();
+    });
+  });
+  it('should have a buildPaths property', function(){
+    assert.isDefined(manifest.buildPaths);
+  });
+  it('should have a globs property', function(){
+    assert.isDefined(manifest.globs);
+  });
+});
